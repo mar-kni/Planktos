fee5c745d05f4b01c20eade0e1301d2d519d8560
Margaret Knight <mknigh21@vols.utk.edu>
2025-10-15 12:27:10 -0400

    changed to Environment and Swarm and changed get_positions to apply_agent_model

--- examples/MK-torque/r-o-a-threezone/threezone.py
+++ examples/MK-torque/r-o-a-threezone/threezone.py
@@ -14,7 +14,7 @@ from cycler import cycler
 import pdb
 
 #set domain to be square with periodic boundary conditions
-envir = planktos.environment(Lx=1, Ly=1, x_bndry='periodic', y_bndry='periodic')
+envir = planktos.Environment(Lx=1, Ly=1, x_bndry='periodic', y_bndry='periodic')
 
 #define fluid flow field 
 #start with no flow and then change to a simple flow scenario later
@@ -30,7 +30,7 @@ envir.flow = [X_flow, Y_flow] #flow is a list of ndarrays
 SWARM_SIZE = 10 #N=2
 
 
-class three_zone_torque(planktos.swarm):
+class three_zone_torque(planktos.Swarm):
     def __init__(self, *args, **kwargs):
         super(three_zone_torque, self).__init__(*args, **kwargs)
         ##### Parameter choices #####
@@ -323,7 +323,7 @@ class three_zone_torque(planktos.swarm):
         return updated_states
     
     
-    def get_positions(self, dt, params=None):
+    def apply_agent_model(self, dt, params=None):
         ''' A method that updates the positions of the swarm based on the ODEs.
         
             Parameters
@@ -400,7 +400,7 @@ for ii in range(3000): ##was 200
     df.loc[len(df)] = {'iteration': ii, 'speed': speed}
 
 #save the df to a csv file- make sure speed is constant
-df.to_csv('examples/MK-torque/r-o-a-one_zone/speed_history_one_zone-corrected10.csv')
+df.to_csv('examples/MK-torque/r-o-a-threezone/speed_history_torque-threezone.csv')
 
 #plot distances between partciles across time  
 #distances = pd.DataFrame(swrm.__calc_dist(swrm.positions[0,:], swrm.positions)) #can't use __calc_dist directly, so use a wrapper function
